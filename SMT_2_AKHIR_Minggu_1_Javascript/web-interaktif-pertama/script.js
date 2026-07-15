77ab99eb1c5480e79f1ac8833ebf429b093745d1
Noth1ng <tegarpermanaputra21@gmail.com>
2026-07-16 04:33:22 +0700

    add another alert

---
+++ SMT_2_AKHIR_Minggu_1_Javascript/web-interaktif-pertama/script.js
@@ -0,0 +1,7 @@
+// Menampilkan pesan di Console
+console.log("Nama Siswa: Tegar");
+
+// Fungsi untuk menampilkan popup alert
+function tampilkanPesan() {
+  alert("Halo! Selamat datang di website ini.");
+}
